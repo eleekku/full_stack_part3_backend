74187abfaf27d572a2a241a9c5baf98b8a189cef
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2025-03-14 10:47:28 +0200

    frontend in production

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors())
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 let persons = [
--- puhelinluettelo/src/components/Persons.jsx
+++
@@ -1,8 +0,0 @@
-const Persons = ({ person, removePerson }) => {
-	return (
-	  <li>{person.name} {person.number}
-	  <button onClick={() => removePerson(person.id)}>delete</button></li>
-	)
-}
-
-export default Persons
