b7e1bddfe6faeaa4915c1684f4f9573e7bac55ab
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2025-04-01 13:17:41 +0300

    frontend working with database ready for deploy

--- models/person.js
+++ models/person.js
@@ -15,17 +15,11 @@ const personSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
-		minlength: 3
 	},
 	number: {
 		type: String,
 		required: true,
-		minlength: 8
 	},
-	_id: {
-		type: mongoose.Schema.Types.ObjectId,
-		auto: true
-	}
 })
 const Person = mongoose.model('Person', personSchema)
 module.exports = Person
