4c320edfcfe21c0a67f28c397a23b523f37c809c
Elias Salmela <144835604+eleekku@users.noreply.github.com>
2025-04-07 18:20:37 +0300

    validation and error in middleware

--- index.js
+++ index.js
@@ -26,6 +26,22 @@ app.use(morgan('tiny', {
   skip: (req) => req.method === 'POST'
 }))
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Weolcome to phonebook!</h1>')
 })
@@ -38,54 +54,82 @@ app.get('/info', (request, response) => {
 	  <p>${date}</p>`
 	)
   })
-})
-
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(result => {
-  response.json(result)
-})
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id	
-  Person.findById(id).then(person => {
-	if (person) {
-	  response.json(person)
-	} else {
-	  response.status(404).end()
-	}
+  .catch(error => {
+    console.error(error)
+    response.status(500).send({ error: 'malformatted id' })
   })
 })
 
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(result => {
+  response.json(result)
+  }
+  )
+  .catch(error => next(error))
+})
 
-app.delete('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id	
+  Person.findById(id).then(person => {
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+  })
+  .catch(error => next(error))
+ 
+})
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   console.log('delete', id)
   Person.findByIdAndDelete(id).then(result => {
-	if (result) {
-	  console.log('deleted', result)
-	}
-	else {
-	  console.log('not found')
-	  return response.status(404).end()
-	}
+  if (result) {
+    console.log('deleted', result)
+  }
+  else {
+    console.log('not found')
+    return response.status(404).end()
+  }
   })
-  response.status(204).end()
+  .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
   const person = new Person({
-	name: body.name,
-	number: body.number,
+  name: body.name,
+  number: body.number,
   })
 
   person.save().then(savedPerson => {
-	response.json(savedPerson)
+  response.json(savedPerson)
   })
-  .catch(error => {
-	console.log('error', error)
-	response.status(400).json({ error: 'Name or number missing' })
-  })
+  .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const { name, number } = request.body
+  if (!name || !number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+  const updatedPerson = { name, number }
+  Person.findByIdAndUpdate(id, updatedPerson, { new: true, runValidators: true })
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -15,10 +15,24 @@ const personSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
+		minlength: 3,
+		validate: {
+			validator: function(v) {
+				return /^[a-zA-Z\s]+$/.test(v)
+			},
+			message: props => `${props.value} is not a valid name!`
+		}
 	},
 	number: {
 		type: String,
 		required: true,
+		minlength: 8,
+		validate: {
+			validator: function(v) {
+				return /\d{2,3}-\d+/.test(v)
+			},
+			message: props => `${props.value} is not a valid phone number!`
+		}
 	},
 })
 const Person = mongoose.model('Person', personSchema)
